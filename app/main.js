7c36e2c7924232b8e9748c057094933ec8befc6b
Onur <onatsurmeli@gmail.com>
2023-12-22 02:38:03 +0100

    add request split functionality and path response conditions

--- app/main.js
+++ app/main.js
@@ -6,7 +6,14 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
-        socket.write('HTTP/1.1 200 OK\r\n\r\n')
+        let request_split = data.toString().split("\r\n");
+        let request_path = request_split[0].split(" ")[1];
+        if (request_path == "/"){
+            socket.write('HTTP/1.1 200 OK\r\n\r\n')
+        }
+        else{
+            socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+        }   
     })
     socket.on("close", () => {
         socket.end();
