ab89624e6632785f6badb5c7de4c02663a47a131
Onur <onatsurmeli@gmail.com>
2023-12-23 19:49:36 +0100

    add console log to check retrieved info

--- app/main.js
+++ app/main.js
@@ -13,7 +13,9 @@ const server = net.createServer((socket) => {
             console.log(request_path)
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${response_body.length}\r\n\r\n${response_body}`);
         } else if (request_path.endsWith("/user-agent")){
+            console.log(request_split[(request_split.length)-1])
             let request_user_agent = request_split.pop().split(" ")[1];
+            console.log(request_user_agent)
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${request_user_agent.length}\r\n\r\n${request_user_agent}`);
         }
         else{
