266c2ada4973596d1fd75aa187dfaecee30ab2f3
Onur <onatsurmeli@gmail.com>
2023-12-23 19:44:25 +0100

    change net import to ES6, added user agent check and response

--- app/main.js
+++ app/main.js
@@ -1,7 +1,4 @@
-const net = require("net");
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+import net from "net";
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
@@ -15,6 +12,9 @@ const server = net.createServer((socket) => {
             console.log(request_split)
             console.log(request_path)
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${response_body.length}\r\n\r\n${response_body}`);
+        } else if (request_path == ("/user-agent")){
+            let request_user_agent = request_split.pop().split(" ")[1];
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${request_user_agent.length}\r\n\r\n${request_user_agent}`);
         }
         else{
             socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
