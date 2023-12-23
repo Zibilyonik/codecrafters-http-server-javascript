7ca27c5339a6fbb16cd7cfff973b8652b096c9ab
Onur <onatsurmeli@gmail.com>
2023-12-23 20:01:28 +0100

    add condition check to loop to fish out user agent

--- app/main.js
+++ app/main.js
@@ -4,9 +4,13 @@ const net = require("net");
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
         let request_split = data.toString().split("\r\n");
+        let request_user_agent = "";
         for(let i = 0; i < request_split.length; i++){
             if (request_split[i] == ""){
                 request_split.splice(i, 1);
+            }
+            if (request_split[i].startsWith("User-Agent:")){
+                request_user_agent = request_split[i].slice(12);
             }
         }
         let request_path = request_split[0].split(" ")[1];
@@ -19,8 +23,6 @@ const server = net.createServer((socket) => {
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${response_body.length}\r\n\r\n${response_body}`);
         } else if (request_path.endsWith("/user-agent")){
             console.log(request_split + "is the array")
-            let request_user_agent = request_split[request_split.length - 1];
-            console.log(request_user_agent)
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${request_user_agent.length}\r\n\r\n${request_user_agent}`);
         }
         else{
